0073be5a2ce75867798b966dd74498bf93fe30f2
Ubuntu <ubuntu@ip-172-31-40-167.us-west-2.compute.internal>
2016-10-25 20:06:23 +0000

    single branch done

--- probc/runtime.sh
+++ probc/runtime.sh
@@ -1,5 +1,6 @@
 echo "Probablistic C runtime" > results.txt
-for model in big-hmm linear-gaussian marsaglia tricky-coin simple-branching; do
+for model in marsaglia tricky-coin simple-branching; do
+#for model in big-hmm linear-gaussian marsaglia tricky-coin simple-branching; do
   printf "$model exps starts\n" >> results.txt
   for i in `seq 1 10`; do
     echo "model: $model, i: $i"
