a4146ce596c1cdb9cd31869e447f6390db40179d
Kai Xu <xukai921110@gmail.com>
2016-10-25 22:11:14 +0100

    fix a typo

--- ProbablisticC/runtime_config.jl
+++ ProbablisticC/runtime_config.jl
@@ -1,7 +1,7 @@
 CONFIG = {
   "batch_size" => 2,
   "num" => 10,
-  "model_list" => ["big-hmm", "linear-gaussian", "marsaglia", "simple-branchinging", "tricky-coin"],
+  "model_list" => ["big-hmm", "linear-gaussian", "marsaglia", "simple-branching", "tricky-coin"],
   "sampler" => "smc",
   "result_file" => "runtime_result.txt"
 }
