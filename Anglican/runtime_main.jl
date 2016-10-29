3ab33d2f9287bf2731fe3b8bfb484bc6c96f1d78
Kai Xu <xukai921110@gmail.com>
2016-10-29 13:26:50 +0100

    test mode

--- Anglican/runtime_main.jl
+++ Anglican/runtime_main.jl
@@ -27,12 +27,14 @@ open(CONFIG["result_file"], "w") do f
       options = string("\"", ":number-of-particles ", num, "\"")
       clojure_command = "(+ 1 1) (m! $model -a $sampler -n 1 -o $options)"
       timing_command = string("(time ", clojure_command, ")")
-      float(readall(pipeline(
+      # float(
+        readall(pipeline(
                            `echo $timing_command`
                          , `lein repl`
                          , `grep 'Elapsed time'`
-                         , `egrep -o '[0-9]*\.[0-9]*'`
-                         ))) / 1000
+                        #  , `egrep -o '[0-9]*\.[0-9]*'`
+                         ))
+                        #  ) / 1000
     end
 
     println("[runtime_main] current model: $(model)")
@@ -40,26 +42,29 @@ open(CONFIG["result_file"], "w") do f
     # 10k
     times_10k = Float64[]
     for i in 1:CONFIG["batch_size"]
-      println("[runtime_main] $(model) 10k $i-th run starts")
+      println("[runtime_main] $(model) 10k $i-th run starts @$(time())")
       push!(times_10k, run_anglican(CONFIG["kilo_num"]))
-      println("[runtime_main] $(model) 10k $i-th run finished with $(times_10k[end])s")
+      println("[runtime_main] $(model) 10k $i-th run finished @$(time()) with $(times_10k[end])s")
     end
 
     write(f, "[$(model)_10k] times: ")
     write(f, "$(times_10k)\n")
-    write(f, "[$(model)_10k] mean: $(mean(times_10k)), std: $(std(times_10k))\n")
+    if CONFIG["batch_size"] > 1
+      write(f, "[$(model)_10k] mean: $(mean(times_10k)), std: $(std(times_10k))\n")
+    end
 
     # 1M
     times_1M = Float64[]
     for i in 1:CONFIG["batch_size"]
-      println("[runtime_main] $(model) 1M $i-th run starts")
+      println("[runtime_main] $(model) 1M $i-th run starts @$(time())")
       push!(times_1M, run_anglican(CONFIG["mili_num"]))
-      println("[runtime_main] $(model) 1M $i-th run finished with $(times_1M[end])s")
+      println("[runtime_main] $(model) 1M $i-th run finished @$(time()) with $(times_1M[end])s")
     end
     write(f, "[$(model)_1M] times: ")
     write(f, "$(times_1M)\n")
-    write(f, "[$(model)_1M] mean: $(mean(times_1M)), std: $(std(times_1M))\n")
-
+    if CONFIG["batch_size"] > 1
+      write(f, "[$(model)_1M] mean: $(mean(times_1M)), std: $(std(times_1M))\n")
+    end
   end
 
 end
