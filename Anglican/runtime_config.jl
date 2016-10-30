72e1f796a1422ba2100f0a615f4cb7311a1e8826
Ubuntu <ubuntu@ip-172-31-40-167.us-west-2.compute.internal>
2016-10-30 20:29:18 +0000

    ms

--- Anglican/runtime_config.jl
+++ Anglican/runtime_config.jl
@@ -1,9 +1,9 @@
 CONFIG = {
-  "batch_size" => 1,
+  "batch_size" => 4,
   "kilo_num" => 10000,
   "mili_num" => 1000000,
- # "model_list" => ["hmm", "gaussian", "marsaglia", "branching", "coin"],
-  "model_list" => ["hmm", "marsaglia", "branching", "coin"],
+  "model_list" => ["hmm", "gaussian", "marsaglia", "branching", "coin"],
+#  "model_list" => ["hmm", "marsaglia", "branching", "coin"],
   "sampler" => "smc",
   "result_file" => "runtime_result.txt"
 }
