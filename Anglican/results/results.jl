8c479e13da80eb96a7ea2b27213bd202b5b78cfa
Kai Xu <xukai921110@gmail.com>
2016-11-03 12:13:12 +0000

    add ang result stats

---
+++ Anglican/results/results.jl
@@ -0,0 +1,22 @@
+ang_hmm_10k = [247843, 249113, 244553, 247733, 243152]
+mean(ang_hmm_10k)
+std(ang_hmm_10k)
+ang_hmm_1M = [2.24870e7, 2.2855e7, 2.3145e7, 2.2078e7, 2.21527e7]
+mean(ang_hmm_1M)
+std(ang_hmm_1M)
+ang_gauss_10k = [61725, 61150, 63634, 61590, 60723]
+mean(ang_gauss_10k)
+ang_gauss_1M = [6497743, 6400086, 6767855, 6196765, 6653613]
+mean(ang_gauss_1M)
+ang_marsaglia_10k = [693, 725, 705, 697, 737, 739]
+mean(ang_marsaglia_10k)
+ang_marsaglia_1M = [4783, 4814, 4322, 4233, 4560, 4440]
+mean(ang_marsaglia_1M)
+# ang_branching_10k = []
+# mean(ang_branching_10k)
+# ang_branching_1M = []
+# mean(ang_branching_1M)
+ang_coin_10k = [115541, 116857, 114385, 115899, 119551, 118906]
+mean(ang_coin_10k)
+# ang_coin_1M = [3195.149490483,3181.427907004,2982.071592413,3065.611161048,3157.643570542,3207.382543152,3202.332671491,3114.402819828,3095.594390369,3125.532138743]
+# mean(ang_coin_1M)
