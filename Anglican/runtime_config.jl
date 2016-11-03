2959003afceb04a96078ab41167df0e82c790dd5
Ubuntu <ubuntu@ip-172-31-40-167.us-west-2.compute.internal>
2016-11-03 23:39:16 +0000

    branching redo done

--- Anglican/runtime_config.jl
+++ Anglican/runtime_config.jl
@@ -1,8 +1,8 @@
 CONFIG = {
-  "batch_size" => 4,
+  "batch_size" => 6,
   "kilo_num" => 10000,
   "mili_num" => 1000000,
-  "model_list" => ["hmm", "gaussian", "marsaglia", "branching", "coin"],
+  "model_list" => ["branching"],
 #  "model_list" => ["hmm", "marsaglia", "branching", "coin"],
   "sampler" => "smc",
   "result_file" => "runtime_result.txt"
