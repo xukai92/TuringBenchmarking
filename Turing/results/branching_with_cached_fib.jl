b2bf440797a43b441437e1fd456748291c58b9d6
Ubuntu <ubuntu@ip-172-31-40-167.us-west-2.compute.internal>
2016-11-07 22:55:36 +0000

    run branching again

---
+++ Turing/results/branching_with_cached_fib.jl
@@ -0,0 +1,14 @@
+[branching_10k] times: [9.191466219,8.168382106,8.30409003,8.209806227,8.270449062,8.411935476,8.274911204,8.169407815,8.365990388,8.391033993]
+[branching_10k] mean: 8.375747252, std: 0.2993583818767433
+[runtime_main] branching 1M 2-th run finished with 2592.023453418s
+[runtime_main] branching 1M 3-th run starts
+[runtime_main] branching 1M 3-th run finished with 2542.42319032s
+[runtime_main] branching 1M 4-th run starts
+[runtime_main] branching 1M 4-th run finished with 2537.186488249s
+[runtime_main] branching 1M 5-th run starts
+[runtime_main] branching 1M 5-th run finished with 2534.473205361s
+[runtime_main] branching 1M 6-th run starts
+[runtime_main] branching 1M 6-th run finished with 2577.120316163s
+[runtime_main] branching 1M 7-th run starts
+[runtime_main] branching 1M 7-th run finished with 2588.522409982s
+[runtime_main] branching 1M 8-th run starts
