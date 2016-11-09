e32307dd49179dd86002b1337c81000e02457fe6
Ubuntu <ubuntu@ip-172-31-40-167.us-west-2.compute.internal>
2016-11-09 09:25:58 +0000

    Finish exps

--- Turing/runtime_config.jl
+++ Turing/runtime_config.jl
@@ -1,10 +1,10 @@
 CONFIG = {
-  "batch_size" => 10,
+  "batch_size" => 5,
   "kilo_num" => 10000,
   "mili_num" => 1000000,
   "obs_num" => 500,
 #  "model_list" => ["big_hmm", "linear_gaussian_1d", "marsaglia", "branching", "tricky_coin"],
-  "model_list" => ["branching"],
+  "model_list" => ["marsaglia"],
   "sampler" => "SMC",
   "result_file" => "runtime_result.txt"
 }
